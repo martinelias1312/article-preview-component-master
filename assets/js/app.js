e6806cef2d2ed17a045c704bdd1d80e3e37e9835
smradupan <martinelias630@gmail.com>
2022-02-02 14:39:11 +0100

    added socials to hide fn

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,6 @@ let share = document.querySelector('.icon-a');
 share.addEventListener('click', hideElements);
 
 function hideElements() {
-    let hide = document.querySelectorAll('.avatar-wrap, .caption-wrap'); 
+    let hide = document.querySelectorAll('.avatar-wrap, .caption-text, .socials'); 
     hide.forEach(item => item.classList.toggle('sr-only'));   
 }
