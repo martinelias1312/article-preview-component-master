f05e9a604af3ef3e2a246a256e2f62b7eef5debc
smradupan <martinelias630@gmail.com>
2022-02-02 15:33:03 +0100

    color toggle added to fn hideElem.

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,4 +5,7 @@ share.addEventListener('click', hideElements);
 function hideElements() {
     let hide = document.querySelectorAll('.avatar-wrap, .caption-text, .socials'); 
     hide.forEach(item => item.classList.toggle('sr-only'));   
+
+    let color = document.querySelector('figure');
+    color.classList.toggle('color');
 }
