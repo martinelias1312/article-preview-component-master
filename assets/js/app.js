e8311e6c9a0a8cab8132f570e48a702ced053a73
smradupan <martinelias630@gmail.com>
2022-02-03 22:16:33 +0100

    developement done!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,20 @@
 'use strict';
-let share = document.querySelector('.icon-a');
-share.addEventListener('click', hideElements);
+let arrow = document.querySelector('.share-btn');
+let svg = document.querySelector('.svg-path');
 
-function hideElements() {
-    let hide = document.querySelectorAll('.avatar-wrap, .caption-text, .socials'); 
-    hide.forEach(item => item.classList.toggle('sr-only'));   
+arrow.addEventListener('click', activate);
 
-    let color = document.querySelector('figure');
-    color.classList.toggle('color');
+function activate() {
+    let popup = document.querySelector('.socials');
+    popup.classList.toggle('active');
 }
+
+arrow.addEventListener("mouseover", () => {
+    svg.classList.add("svg-hover");
+    arrow.classList.add("share-btn-hover");
+});
+
+arrow.addEventListener("mouseout", () => {
+    svg.classList.remove("svg-hover");
+    arrow.classList.remove("share-btn-hover");
+});
